06ea4355c2c92696eaf4cdc03641750763b95468
于超 <chao.yu@jinkaimen.com>
2017-02-28 11:24:15 +0800

    提交

--- express_static_server/static_server/routes/index.js
+++ express_static_server/static_server/routes/index.js
@@ -5,6 +5,8 @@ var fs = require('fs');
 
 /* GET home page. */
 router.get('*', function (req, res, next) {
+  console.log(1);
+  console.log(req);
   res.send(fs.readFileSync('./dist/index.html', 'utf8'));
 });
 
